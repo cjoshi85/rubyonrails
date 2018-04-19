52d1290b309e5e613ca0dfa533474c2f91e8ba2f
chetan joshi <cjoshi85@gmail.com>
2018-04-19 03:52:49 +0000

    Added user_id to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    
+    has_many :articles
     validates :username,presence: true,length: {minimum:3,maximum:25},uniqueness: {case_sensitive: false}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,presence: true,length: {maximum:100},uniqueness: {case_sensitive: false},
