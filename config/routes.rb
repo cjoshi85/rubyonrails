58eaa46dfdc07c69d7fe9530eac424f861c93171
chetan joshi <cjoshi85@gmail.com>
2018-04-22 01:43:09 +0000

    Added user articles and show

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'about', to: 'welcome#about'
+  
+  get 'signup',to: 'users#new'
+  
+  resources :users, except:[:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
